dccd6d3018adf888ae83d7654732a51ed9878ba3
Jonathan To <jonto398@gmail.com>
2023-05-22 02:37:37 -0400

    Added bow and arrow scripts

--- ArcherFPS/Assets/Scripts/Bow.cs
+++ ArcherFPS/Assets/Scripts/Bow.cs
@@ -12,10 +12,63 @@ public class Bow : MonoBehaviour
 
     [SerializeField]
     float delay;
+
+    [SerializeField]
+    float arrowPower;
+
+    [SerializeField]
+    [Range(0, 3)]
+    float zoomSpeed;
+
     Arrow currentArrow;
+    bool isAiming;
+    bool zoomedIn;
+    bool fire;
+    Camera cam;
+    float startingFOV;
 
-    public void Fire(float firePower)
+    void Start()
     {
+        cam = Camera.main;
+        startingFOV = cam.fieldOfView;
+    }
 
+    void Update()
+    {
+        GetAimInput();
+
+        if (isAiming)
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 80f, Time.deltaTime * zoomSpeed);
+        }
+        else if (fire)
+        {
+            FireArrow();
+        }
+        else
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, startingFOV, 0.1f);
+        }
+
+    }
+
+    void GetAimInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isAiming = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isAiming = false;
+            fire = true;
+        }
+    }
+
+    void FireArrow()
+    {
+        currentArrow = Instantiate(arrowPrefab, arrowSpawnPoint);
+        currentArrow.Fly(arrowSpawnPoint.forward * arrowPower);
+        fire = false;
     }
 }
