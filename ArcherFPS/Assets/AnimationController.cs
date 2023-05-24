c5f9853eb24a6b1dd53c46b4fa8eb70b2b779785
Jonathan To <jonto398@gmail.com>
2023-05-24 19:09:18 -0400

    Set up initial animation components + crosshair UI

---
+++ ArcherFPS/Assets/AnimationController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
